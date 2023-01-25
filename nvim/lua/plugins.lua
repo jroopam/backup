48cd081008911bdbf5345e2143b2ead31943126a
jroopam <59415105+jroopam@users.noreply.github.com>
2023-01-25 22:54:48 +0530

    Update plugins.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,15 +1,10 @@
-local packer = require("packer")
+local packer = require('packer')
 
 packer.startup(function(use)
-	use({ "wbthomason/packer.nvim" })
-
-	use({
-		"nvim-telescope/telescope.nvim",
-		requires = { "nvim-lua/plenary.nvim" },
-		cmd = "Telescope",
-	})
-
-    use("nvim-treesitter/nvim-treesitter", {
-        run = ":TSUpdate"
-    })
+	use 'wbthomason/packer.nvim'
+	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+	use {
+		'nvim-telescope/telescope.nvim',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
 end)

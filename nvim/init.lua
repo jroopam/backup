4f81d936d41ac6011df79c8ba1b992b45f917e30
jroopam <59415105+jroopam@users.noreply.github.com>
2023-01-25 23:34:11 +0530

    Update and rename init.vim to init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,36 @@
+vim.cmd([[
+set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath = &runtimepath
+source ~/.vimrc
+]])
+
+require("plugins")
+
+-- Copied from nvim-tree.lua Github repo
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+-- OR setup with some options
+require("nvim-tree").setup({
+	sort_by = "case_sensitive",
+	view = {
+		mappings = {
+			list = {
+				{ key = "u", action = "dir_up" },
+			},
+		},
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = true,
+	},
+}

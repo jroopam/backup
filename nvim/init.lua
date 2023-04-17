99e1892174ff7e595b01f4e5b671b1a19436a2d8
jroopam <59415105+jroopam@users.noreply.github.com>
2023-04-17 23:04:56 +0530

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -224,28 +224,3 @@ keyset("n", "<space>k", ":<C-u>CocPrev<cr>", opts)
 keyset("n", "<space>p", ":<C-u>CocListResume<cr>", opts)
 
 vim.o.background = "dark" -- or "light" for light mode
---vim.cmd([[colorscheme gruvbox]])
--- setup must be called before loading the colorscheme
--- Default options:
-require("gruvbox").setup({
-	undercurl = true,
-	underline = true,
-	bold = true,
-	italic = {
-     strings = true,
-     operators = true,
-     comments = true,
-  	},
-	strikethrough = true,
-	invert_selection = false,
-	invert_signs = false,
-	invert_tabline = false,
-	invert_intend_guides = false,
-	inverse = true, -- invert background for search, diffs, statuslines and errors
-	contrast = "", -- can be "hard", "soft" or empty string
-	palette_overrides = {},
-	overrides = {},
-	dim_inactive = false,
-	transparent_mode = false,
-})
-vim.cmd("colorscheme gruvbox")

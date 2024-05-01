45e9b92f4bd8eab50a9b2a46c4084f27d8983325
Roopam <jroopam.792@gmail.com>
2024-05-01 22:25:07 +0530

    remove test line

--- scripts/remove_env_variables.sh
+++ scripts/remove_env_variables.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
+# This scipt should run when the computer shuts down, but as tmux is its environment so when you do tmux kill-server it executes this scirpt
 sed -i '/export TMUX_STARTED=1/d' ~/.zshenv
-touch chli

3667dae02951bffafcecbcf8300583694e487a39
Roopam <jroopam.792@gmail.com>
2024-08-05 22:04:23 +0530

    add blanklink indent plugin

--- nvim/lua/plugins/enhancement.lua
+++ nvim/lua/plugins/enhancement.lua
@@ -34,6 +34,31 @@ return {
     {
         'aluriak/nerdcommenter'
     },
+    {
+        "lukas-reineke/indent-blankline.nvim", main = "ibl",
+        opts = {
+            indent = {
+                char = "│",
+                tab_char = "│",
+            },
+            scope = { show_start = false, show_end = false },
+            exclude = {
+                filetypes = {
+                    "help",
+                    "alpha",
+                    "dashboard",
+                    "neo-tree",
+                    "Trouble",
+                    "trouble",
+                    "lazy",
+                    "mason",
+                    "notify",
+                    "toggleterm",
+                    "lazyterm",
+                },
+            },
+        }
+    },
 }
 
 

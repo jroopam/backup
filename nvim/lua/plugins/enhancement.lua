d3dae9eaa53ad5a33a57afe652327fa7903e2de0
Roopam <jroopam.792@gmail.com>
2024-12-28 14:56:24 +0530

    add markview

--- nvim/lua/plugins/enhancement.lua
+++ nvim/lua/plugins/enhancement.lua
@@ -52,6 +52,115 @@ return {
             },
         }
     },
+    {
+        "OXY2DEV/markview.nvim",
+        lazy = false,      -- Recommended
+        -- ft = "markdown" -- If you decide to lazy-load anyway
+
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-tree/nvim-web-devicons"
+        },
+        config = function()
+            require("markview").setup({
+                hybrid_modes = { "n" },
+                code_blocks = {
+                    enable = true,
+
+                    --- Icon provider for the block icons & signs.
+                    ---
+                    --- Possible values are,
+                    ---   • "devicons", Uses `nvim-web-devicons`.
+                    ---   • "mini", Uses `mini.icons`.
+                    ---   • "internal", Uses the internal icon provider.
+                    ---   • "", Disables icons
+                    ---
+                    ---@type "devicons" | "mini" | "internal" | ""
+                    icons = "internal",
+
+                    --- Render style for the code block.
+                    ---
+                    --- Possible values are,
+                    ---   • "simple", Simple line highlighting.
+                    ---   • "minimal", Box surrounding the code block.
+                    ---   • "language", Signs, icons & much more.
+                    ---
+                    ---@type "simple" | "minimal" | "language"
+                    style = "language",
+
+                    --- Primary highlight group.
+                    --- Used by other options that end with "_hl" when
+                    --- their values are nil.
+                    ---@type string
+                    hl = "MarkviewCode",
+
+                    --- Highlight group for the info string
+                    ---@type string
+                    info_hl = "MarkviewCodeInfo",
+
+                    --- Minimum width of a code block.
+                    ---@type integer
+                    min_width = 40,
+
+                    --- Left & right padding amount
+                    ---@type integer
+                    pad_amount = 0,
+
+                    --- Character to use as whitespace
+                    ---@type string?
+                    pad_char = " ",
+
+                    --- Table containing various code block language names
+                    --- and the text to show.
+                    --- e.g. { cpp = "C++" }
+                    ---@type { [string]: string }
+                    language_names = nil,
+
+                    --- Direction of the language preview
+                    ---@type "left" | "right"
+                    language_direction = "left",
+
+                    --- Enables signs
+                    ---@type boolean
+                    sign = true,
+
+                    --- Highlight group for the sign
+                    ---@type string?
+                    sign_hl = nil
+                },
+                inline_codes = {
+                    enable = true,
+                    hl = "MarkviewHeading3",
+
+                    --- Left corner, Added before the left padding.
+                    ---@type string?
+                    corner_left = nil,
+
+                    --- Left padding, Added before the text.
+                    ---@type string?
+                    padding_left = nil,
+
+                    --- Right padding, Added after the text.
+                    ---@type string?
+                    padding_right = nil,
+
+                    --- Right corner, Added after the right padding.
+                    ---@type string?
+                    corner_right = nil,
+
+                    ---@type string?
+                    corner_left_hl = nil,
+                    ---@type string?
+                    padding_left_hl = nil,
+
+                    ---@type string?
+                    padding_right_hl = nil,
+                    ---@type string?
+                    corner_right_hl = nil,
+                }
+            });
+        end
+    },
     -- {
     --     'echasnovski/mini.animate',
     --     config = true,

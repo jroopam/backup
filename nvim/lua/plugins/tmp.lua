a2a1449be6e77137dc85232b6b8003ab3a837797
Roopam <jroopam.792@gmail.com>
2025-04-04 13:40:30 +0530

    add some plugins

---
+++ nvim/lua/plugins/tmp.lua
@@ -0,0 +1,23 @@
+-- lazy.nvim
+return {
+    {
+        "m4xshen/hardtime.nvim",
+        dependencies = { "MunifTanjim/nui.nvim" },
+        opts = {
+            max_time = 1200,
+            max_count = 1;
+        }
+    },
+    {
+        'rmagatti/auto-session',
+        lazy = false,
+
+        ---enables autocomplete for opts
+        ---@module "auto-session"
+        ---@type AutoSession.Config
+        opts = {
+            suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+            -- log_level = 'debug',
+        }
+    }
+}

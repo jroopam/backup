19557586abafb2b73e9cb53a529316a69c68791f
Roopam <jroopam.792@gmail.com>
2026-04-02 23:41:10 +0530

    Experimental UI2: Enhancements

--- nvim/init.lua
+++ nvim/init.lua
@@ -225,6 +225,107 @@ vim.api.nvim_create_autocmd("VimEnter", {
     end
 })
 
+-- Prevent Press Enter to Continue Block Start ---
+-- Ref: https://www.reddit.com/r/neovim/comments/1sa95g4/no_more_press_enter_with_ui2_with_example/
+-- Ref: https://github.com/ThorstenRhau/neovim/blob/c5d8652edeedcd64a0e51c236986f379b8ac0724/lua/config/options.lua#L86-L133
+local opt = vim.opt
+local o = vim.o
+
+o.completeopt = 'menu,menuone,noselect'
+o.cursorline = true
+o.expandtab = true
+o.ignorecase = true
+o.linebreak = true
+o.mouse = 'nv'
+o.number = true
+o.numberwidth = 2
+o.pumborder = 'single'
+o.pumheight = 10
+o.showmode = false
+o.smartcase = true
+o.smartindent = true
+o.smoothscroll = true
+o.softtabstop = 4
+o.splitbelow = true
+o.splitright = true
+o.statuscolumn = '%s%=%{v:relnum?v:relnum:v:lnum} '
+o.tabstop = 4
+o.timeoutlen = 300
+o.undofile = true
+o.undolevels = 1000
+o.winborder = 'single'
+
+-- Cursor appearance and blinking
+o.guicursor = table.concat({
+  'n-v-c-sm:block-Cursor', -- Normal, Visual, Command, Showmatch: block cursor
+  'i-ci-ve:ver25-Cursor', -- Insert, Command-insert, Visual-exclusive: vertical bar (25% width)
+  'r-cr-o:hor20-Cursor', -- Replace, Command-replace, Operator-pending: horizontal bar (20% height)
+  'a:blinkwait500-blinkoff500-blinkon500',
+}, ',')
+
+-- Whitespace characters    
+o.list = true
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Folding (treesitter-based, configured per filetype)
+o.foldlevel = 99
+o.foldlevelstart = 99
+
+-- Spelling
+o.spelllang = 'en_us'
+o.spellsuggest = 'best,20' -- Limits to 20 suggestions
+
+-- Experimental UI2: floating cmdline and messages
+o.cmdheight = 0
+require('vim._core.ui2').enable({
+  enable = true,
+  msg = {
+    targets = {
+      [''] = 'msg',
+      empty = 'cmd',
+      bufwrite = 'msg',
+      confirm = 'cmd',
+      emsg = 'pager',
+      echo = 'msg',
+      echomsg = 'msg',
+      echoerr = 'pager',
+      completion = 'cmd',
+      list_cmd = 'pager',
+      lua_error = 'pager',
+      lua_print = 'msg',
+      progress = 'pager',
+      rpc_error = 'pager',
+      quickfix = 'msg',
+      search_cmd = 'cmd',
+      search_count = 'cmd',
+      shell_cmd = 'pager',
+      shell_err = 'pager',
+      shell_out = 'pager',
+      shell_ret = 'msg',
+      undo = 'msg',
+      verbose = 'pager',
+      wildlist = 'cmd',
+      wmsg = 'msg',
+      typed_cmd = 'cmd',
+    },
+    cmd = {
+      height = 0.5,
+    },
+    dialog = {
+      height = 0.5,
+    },
+    msg = {
+      height = 0.3,
+      timeout = 5000,
+    },
+    pager = {
+      height = 0.5,
+    },
+  },
+})
+--- Block End ---
+
+--- Dynamically placed Relative Numbers block start ---
 local ns = vim.api.nvim_create_namespace("relative_eol_numbers")
 local num_enabled = false
 
@@ -350,5 +451,6 @@ vim.keymap.set(
   toggle_relative_numbers,
   { desc = "Toggle virtual relative line numbers" }
 )
+--- Block End ---
 
 require("custom")
